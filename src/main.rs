4339f9fd1c7448168c47fc9516223823f05586d7
Pavel Sedlák <psedlak@redhat.com>
2025-09-15 22:02:44 +0200

    Parallel deletion requests using tokio

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use serde_derive::{Deserialize, Serialize};
 use std::env;
-use reqwest::{Url, StatusCode, blocking::Client};
+use reqwest::{Client, Url, StatusCode};
 
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -15,38 +15,50 @@ pub struct Item {
     #[serde(alias = "uuid")]
     pub id: String,
 }
-fn get_delete_list(url: String, token: String) -> ResponseData{
+async fn get_delete_list(url: String, token: String) -> ResponseData{
     let req_url: Url = Url::parse(&url).expect("Error parsing");
     let client = Client::builder().danger_accept_invalid_certs(true).build().expect("Failed");
     let response = client
         .get(req_url)
         .header("Authorization", format!("Bearer {}", token))
         .send()
+        .await
         .expect("Request failed");
     if response.status()!=StatusCode::OK{
-        println!("Request to get list failed with error code {}", response.status());
+        println!("Request to get list failed with error code {}:\nURL: {}\nToken: {}...",
+            response.status(),
+            url,
+            &token[..5],
+        );
     }
-    let tpares: ResponseData = serde_json::from_str(&response.text().expect("Parsing failed")).expect("Failed");
+    let tpares: ResponseData = serde_json::from_str(
+        &response.text().await.expect("Parsing failed"))
+        .expect("Failed");
     tpares
 }
 
-fn delete_resource(url:String, token: String, delete_list: ResponseData){
+async fn delete_resource(url:String, token: String, delete_list: ResponseData) {
     println!("Deletion Started...");
     println!("Total number of records to be deleted: {}", delete_list.total);
-    for response in delete_list.items{
-        let delete_url = format!("{}/{}",url, response.id);
+    let mut responses = Vec::with_capacity(delete_list.total.try_into().unwrap());
+    for item in delete_list.items {
+        let delete_url = format!("{}/{}",url, item.id);
         let req_url: Url = Url::parse(&delete_url).expect("Error parsing");
         println!("Delete URL {}", &req_url);
         let client = Client::builder().danger_accept_invalid_certs(true).build().expect("Failed");
-        let delete_res = client.delete(req_url).header("Authorization", format!("Bearer {}", token)).send().expect("Failed");
-        if delete_res.status() != StatusCode::OK{
-            println!("Deletion failed for resource: {} with error {}",response.id, delete_res.status());
+        responses.push(client.delete(req_url).header("Authorization", format!("Bearer {}", token)).send());
+    }
+    for response in responses {
+        let delete_res = response.await.expect("Failed");
+        if delete_res.status() != StatusCode::OK {
+            println!("Deletion failed for resource: {} with error {}", delete_res.url(), delete_res.status());
         }
     }
     println!("Deletion Done...");
 }
 
-fn main(){
+#[tokio::main]
+async fn main() {
     let api_url = match env::var("BASE_URL") { // Example https://server.apps.tpa.qe.net/api/v2/advisory
         Ok(url) => url,
         _ => panic!("ERROR: Specify BASE_URL env var (e.g. BASE_URL='http://localhost/api/v2/advisory')"),
@@ -61,8 +73,8 @@ fn main(){
     };
 
     let get_url = format!("{}{}",api_url, filter);
-    let res = get_delete_list(get_url.to_string(), token.to_string());
-    delete_resource(api_url.to_string(), token.to_string(), res);
+    let res = get_delete_list(get_url.to_string(), token.to_string()).await;
+    delete_resource(api_url.to_string(), token.to_string(), res).await;
 
 }
 
